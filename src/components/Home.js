035eadf980178ee1a9fe57619f62f2f2ce4eb66b
tbrown034 <tbrown034@gmail.com>
2022-10-08 13:15:18 -0500

    set up

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+
+function Home() {
+    return (
+      <div className="Home">
+        <header className="Home-header">
+          <h1>Hello World</h1>
+          <h3>Welcome to my Learning Page</h3>
+          <p>Created by Trevor Brown</p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Home;
