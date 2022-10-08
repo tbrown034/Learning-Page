bf3f4e6439fdaf9eeeee87e4254057baae9c6ad4
tbrown034 <tbrown034@gmail.com>
2022-10-08 13:17:47 -0500

    second

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
       <div className="Home">
         <header className="Home-header">
           <h1>Hello World</h1>
-          <h3>Welcome to my Learning Page</h3>
+          <h3>Welcome to my Learning Page!</h3>
           <p>Created by Trevor Brown</p>
         </header>
       </div>
